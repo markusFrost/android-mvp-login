7ac1cb4753fca07d49b4fe67644b3a80c65de840
markusFrost <andreywystawkin@yandex.ru>
2016-01-30 17:52:28 +0400

    prepare project to work

--- AndroidMvpLogin/app/src/main/java/mvp/android/testfor/com/androidmvplogin/LoginActivity.java
+++ AndroidMvpLogin/app/src/main/java/mvp/android/testfor/com/androidmvplogin/LoginActivity.java
@@ -1,5 +1,6 @@
 package mvp.android.testfor.com.androidmvplogin;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +10,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
